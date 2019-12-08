f3fb735370a6a0fa759d7b551dddbf79c25f1fde
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2019-12-08 13:20:37 -0800

    testing return tell()

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ def hello_world():
 
 @app.route('/summary')
 def summary():
-    return requests.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-news", headers={"X-RapidAPI-Key":"29180c2784msha6aad19076e8d65p1dadd5jsn88b8e10ea1cf"},
-                                                                                        params={"category":"NBEV"}).json()
+    # return requests.get("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-news", headers={"X-RapidAPI-Key":"29180c2784msha6aad19076e8d65p1dadd5jsn88b8e10ea1cf"},
+    #        
+    speech = 'Microphone check 1, 2 what is this?'
+    return tell(speech)                                                                             params={"category":"NBEV"}).json()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
